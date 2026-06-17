45b8b7d8228ba9e45ca7dec859e74ccde414c331
Hayley-hash <hayleyosner@gmail.com>
2026-06-17 23:20:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-export default function ...
+function showPage(pageId) {
+  document.querySelectorAll('.page').forEach(p => {
+    p.classList.remove('active');
+  });
+
+  document.getElementById(pageId).classList.add('active');
+}
+
+function saveForm() {
+  alert("Form saved (frontend only for now)");
+}
